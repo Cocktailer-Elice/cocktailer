3463ef3607d43f180f639773fb5c414824e1c21e
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-12-14 18:35:34 +0900

    Setting : BE server.ts / index.ts 세팅

---
+++ Backend/src/server.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+class Server {
+  public app: express.Application;
+
+  constructor() {
+    const app: express.Application = express();
+    this.app = app;
+  }
+
+  private setRoute() {}
+
+  private setMiddleware() {
+    dotenv.config({ path: `${__dirname}/.env` });
+
+    this.app.use(
+      cors({
+        origin: `${process.env.URL}:${process.env.PORT}`,
+        credentials: true,
+      }),
+    );
+
+    this.app.use((req, res, next) => {
+      console.log(`✅ req has been arrived from : ${req.rawHeaders[1]}`);
+      next();
+    });
+
+    //* json middleware
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+
+    this.setRoute();
+  }
+
+  public listen() {
+    this.setMiddleware();
+    this.app.listen(process.env.PORT, () => {
+      console.log(`✅ Server is on : ${process.env.URL}:${process.env.PORT}`);
+      console.log('server.ts');
+      console.log('test');
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
