ef5bec5f351eea8a85636aefc453ffb91a7eb079
JongHoonSon <roka1684@gmail.com>
2022-12-15 16:05:13 +0900

    Chore: 헤더, 홈, 칵고리즘 관련 폴더/파일 생성

---
+++ Frontend/src/containers/Cockgorithm/CockgorithmModal.tsx
@@ -0,0 +1,5 @@
+const CockgorithmModal = () => {
+  return <></>;
+};
+
+export default CockgorithmModal;
---
+++ Frontend/src/containers/Home/HomePageButtonSection.tsx
@@ -0,0 +1,5 @@
+const HomePageButtonSection = () => {
+  return <></>;
+};
+
+export default HomePageButtonSection;
