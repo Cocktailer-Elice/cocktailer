281a29e98bad151d41a7a8cba0dfc0a5c691d66e
BB2e <79270369+BB2e@users.noreply.github.com>
2022-12-15 17:40:06 +0900

    design: 초기 세팅

---
+++ Frontend/src/components/Cockflow/CockflowList.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function CockflowList() {
+  return (
+    <div>CockflowList</div>
+  )
+}
+
+CockflowList.propTypes = {}
+
+export default CockflowList
---
+++ Frontend/src/containers/Cockflow/Cockflow.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Cockflow() {
+  return (
+    <div>Cockflow</div>
+  )
+}
+
+Cockflow.propTypes = {}
+
+export default Cockflow
