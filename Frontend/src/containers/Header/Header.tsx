a6699ea85392e32bf7d193ade8446324904d1a9e
JongHoonSon <roka1684@gmail.com>
2022-12-15 16:55:50 +0900

    Style: App, Header 스타일 추가

--- Frontend/src/containers/Header/Header.tsx
+++ Frontend/src/containers/Header/Header.tsx
@@ -1,5 +1,15 @@
+import styled from 'styled-components';
+
 const Header = () => {
-  return <></>;
+  return <HeaderWrapper>헤더</HeaderWrapper>;
 };
+
+const HeaderWrapper = styled.div`
+  width: 100%;
+  height: 70px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+  box-sizing: border-box;
+`;
 
 export default Header;
