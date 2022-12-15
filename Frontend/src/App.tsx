192266040d01b9ee2a2fa0c89a45093629f498ea
LucetTin5 <lucettin5@gmail.com>
2022-12-15 16:45:34 +0900

    setting: globalStyle setting, Helmet, theme base setting

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,27 @@
 import Router from './Router';
+import { createGlobalStyle } from 'styled-components';
+import { Helmet } from 'react-helmet';
+import { reset } from 'styled-reset';
+
+const GlobalStyles = createGlobalStyle`
+  ${reset}
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+  body {
+    
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Router></Router>
-    </div>
+    <>
+      <Helmet></Helmet>
+      <GlobalStyles></GlobalStyles>
+      <Router></Router>;
+    </>
   );
 }
 
---
+++ Frontend/src/style.d.ts
@@ -0,0 +1,5 @@
+import 'styled-components';
+
+declare module 'stlyed-components' {
+  export interface DefaultTheme {}
+}
