965643cfd8d27b18c6b17bb1275ca4ce327e9bda
siha <es2008e@naver.com>
2022-12-15 16:46:52 +0900

    chore: wrapper 삭제

--- Frontend/src/pages/Cockcipe/CockcipePage.tsx
+++ Frontend/src/pages/Cockcipe/CockcipePage.tsx
@@ -4,21 +4,13 @@ import ApplyContainer from '../../containers/Cockcipe/ApplyContainer';
 import InputContainer from '../../containers/Cockcipe/InputContainer';
 import ListContainer from '../../containers/Cockcipe/ListContainer';
 
-const Wrapper = styled.div`
-  width: 480px;
-  height: auto;
-  margin: 0 auto;
-  border: 1px solid black;
-  padding: 10px;
-`;
-
 const CockcipePage = () => {
   return (
-    <Wrapper>
+    <>
       <InputContainer />
       <ListContainer />
       <ApplyContainer />
-    </Wrapper>
+    </>
   );
 };
 
