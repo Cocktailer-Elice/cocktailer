392ecf1c520739ff88ef0a6b28c9f4a954a6e347
siha <es2008e@naver.com>
2022-12-16 19:14:53 +0900

    Feat: 임시 데이터 추가

--- Frontend/src/components/Cockcipe/List/CocktailApplyBtn.tsx
+++ Frontend/src/components/Cockcipe/List/CocktailApplyBtn.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const CocktailApplyBtn = () => {
-  const handleMoveApply = (event: React.MouseEvent<HTMLDivElement>) => {
-    window.location.href = '/cockcipe/apply';
-  };
-  return <ApplyBtn onClick={handleMoveApply}>나만의 레시피 등록하기</ApplyBtn>;
+  return <ApplyBtn to="/cockcipe/apply">나만의 레시피 등록하기</ApplyBtn>;
 };
 
 export default CocktailApplyBtn;
-const ApplyBtn = styled.div`
+const ApplyBtn = styled(Link)`
   border: 1px solid black;
   width: 100px;
+  text-decoration: none;
 `;
--- Frontend/src/containers/Cockcipe/List/ListContainer.tsx
+++ Frontend/src/containers/Cockcipe/List/ListContainer.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CocktailListItem from '../../../components/Cockcipe/List/CocktailListItem';
 import styled from 'styled-components';
+import axios from 'axios';
 
 // useEffect 데이터 get 후 칵테일 리스트 map 출력
 const ListContainer = () => {
+  useEffect(() => {
+    axios
+      .get('/src/containers/Cockcipe/List/data.json')
+      .then((res) => console.log(res));
+  }, []);
   return (
     <>
       <Category>카테고리 1</Category>
