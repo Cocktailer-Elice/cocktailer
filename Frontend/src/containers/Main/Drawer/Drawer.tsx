1cbc8842cce230a38902ebe1a766aece6bb24003
JongHoonSon <roka1684@gmail.com>
2022-12-16 21:04:42 +0900

    Feat: Drawer 컴포넌트 구성

--- Frontend/src/containers/Main/Drawer/Drawer.tsx
+++ Frontend/src/containers/Main/Drawer/Drawer.tsx
@@ -1,15 +1,33 @@
 import styled from 'styled-components';
 
 export const Drawer = () => {
-  return <DrawerWrapper>Drawer</DrawerWrapper>;
+  return (
+    <DrawerWrapper>
+      <DrawerUserSectionWrapper />
+      <DrawerPageSectionWrapper />
+    </DrawerWrapper>
+  );
 };
 
 const DrawerWrapper = styled.div`
   width: 300px;
   height: 100%;
-  border: 1px solid black;
-  background-color: gray;
+  border: 1px solid gray;
   position: absolute;
   left: 0;
   top: 0;
+`;
+
+const DrawerUserSectionWrapper = styled.div`
+  width: 100%;
+  height: 50px;
+  border: 1px solid gray;
+  background-color: white;
+`;
+
+const DrawerPageSectionWrapper = styled.div`
+  width: 100%;
+  height: calc(100% - 50px);
+  border: 1px solid gray;
+  background-color: white;
 `;
