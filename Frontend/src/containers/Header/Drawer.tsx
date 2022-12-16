93e62d59ab4dc21ddcbc9bb8170136be54c0ab89
JongHoonSon <roka1684@gmail.com>
2022-12-16 19:05:17 +0900

    Feat: Drawer 컴포넌트 구현

--- Frontend/src/containers/Header/Drawer.tsx
+++ Frontend/src/containers/Header/Drawer.tsx
@@ -1,3 +1,15 @@
+import styled from 'styled-components';
+
 export const Drawer = () => {
-  return <div>Drawer</div>;
+  return <DrawerWrapper>Drawer</DrawerWrapper>;
 };
+
+const DrawerWrapper = styled.div`
+  width: 300px;
+  height: 100%;
+  border: 1px solid black;
+  background-color: gray;
+  position: absolute;
+  left: 0;
+  top: 0;
+`;
