f16c179bc9d8f49f027025108789b66a107588ba
JongHoonSon <roka1684@gmail.com>
2022-12-16 22:36:39 +0900

    Feat: Drawer Button 구현

--- Frontend/src/components/Main/Drawer/DrawerPageButton.tsx
+++ Frontend/src/components/Main/Drawer/DrawerPageButton.tsx
@@ -1,3 +1,24 @@
-export const DrawerPageButton = () => {
-  return <div>DrawerPageButton</div>;
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+interface DrawerPageButtonProps {
+  pageName: string;
+  link: string;
+}
+
+export const DrawerPageButton = ({ pageName, link }: DrawerPageButtonProps) => {
+  return (
+    <Link to={link}>
+      <PageButtonWrapper>{pageName}</PageButtonWrapper>
+    </Link>
+  );
 };
+
+const PageButtonWrapper = styled.div`
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 1px solid green;
+`;
