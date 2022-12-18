1412994de857972ce7d808319a67dd67709bac40
BB2e <79270369+BB2e@users.noreply.github.com>
2022-12-18 22:53:45 +0900

    chore: 오타 변경, 파일명 변경

--- Frontend/src/components/Cockflow/CockflowBoxTitle.tsx
+++ Frontend/src/components/Cockflow/CockflowBoxTitle.tsx
@@ -4,20 +4,18 @@ const SmallTitle = styled.div`
   padding: 15px 0; 
   font-weight: bold;
   border-bottom: 1px solid #ddd;
-`
+`;
 
-const CockflowBoxTitle = ({ replied = 0 }) => {
+export const CockflowBoxTitle = ({ replied = 0 }) => {
   return (
     <SmallTitle>
       {
         replied
-        ?
-        `${replied}개의 답변`
-        :
-        '답변 달기' 
+          ?
+          `${replied}개의 답변`
+          :
+          '답변 달기'
       }
     </SmallTitle>
-  )
-}
-
-export default CockflowBoxTitle
+  );
+};
