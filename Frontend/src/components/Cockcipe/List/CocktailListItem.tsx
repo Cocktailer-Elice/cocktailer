1d8f649165d7e2a3960154b170e4b5bc153fb764
siha <es2008e@naver.com>
2022-12-19 12:49:01 +0900

    Feat: Swiper 기능

--- Frontend/src/components/Cockcipe/List/CocktailListItem.tsx
+++ Frontend/src/components/Cockcipe/List/CocktailListItem.tsx
@@ -2,15 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 
 interface Props {
-  id: number;
+  id: string;
+  name: string;
 }
 
-const CocktailListItem = ({ id }: Props) => {
+const CocktailListItem = ({ id, name }: Props) => {
   const handleDetailPage = (event: React.MouseEvent<HTMLDivElement>) => {
     console.log(event);
     window.location.href = `/cockcipe/detail`;
   };
-  return <ThumbnailBox onClick={handleDetailPage}>레시피 사진</ThumbnailBox>;
+  return (
+    <ThumbnailBox onClick={handleDetailPage}>
+      레시피 사진<p>{id}</p>
+    </ThumbnailBox>
+  );
 };
 
 export default CocktailListItem;
--- Frontend/src/containers/Cockcipe/List/ListContainer.tsx
+++ Frontend/src/containers/Cockcipe/List/ListContainer.tsx
@@ -1,23 +1,46 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CocktailListItem from '../../../components/Cockcipe/List/CocktailListItem';
 import styled from 'styled-components';
 import axios from 'axios';
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper';
+import 'swiper/css'; //basic
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 // useEffect 데이터 get 후 칵테일 리스트 map 출력
+interface Data {
+  name: string;
+  img: string;
+  id: string;
+  category: string;
+}
 const ListContainer = () => {
+  const [cockList, setCockList] = useState<Data[]>([]);
   useEffect(() => {
-    axios
-      .get('/src/containers/Cockcipe/List/data.json')
-      .then((res) => console.log(res));
+    axios.get('/src/containers/Cockcipe/List/data.json').then((res) => {
+      console.log(res.data.data);
+      res.data.data.map((item: Data) => {
+        console.log(item);
+        setCockList((prev) => [...prev, item]);
+      });
+    });
   }, []);
   return (
     <>
       <Category>카테고리 1</Category>
-      <Container>
-        <CocktailListItem id={1} />
-        <CocktailListItem id={2} />
-        <CocktailListItem id={3} />
-      </Container>
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={10}
+        navigation
+        loop={true}
+        modules={[Navigation]}
+      >
+        {cockList?.map((item, idx) => (
+          <SwiperSlide key={idx}>
+            <CocktailListItem key={idx} name={item.name} id={item.id} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </>
   );
 };
@@ -25,8 +48,4 @@ const ListContainer = () => {
 export default ListContainer;
 const Category = styled.p`
   font-size: 15px;
-`;
-const Container = styled.div`
-  display: flex;
-  flex-direction: row;
 `;
