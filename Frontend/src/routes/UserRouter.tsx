98d96c84b2c844389f7f7713e1e74fccb3e1aeb9
LucetTin5 <lucettin5@gmail.com>
2022-12-19 22:40:49 +0900

    Chore: Route 수정

--- Frontend/src/routes/UserRouter.tsx
+++ Frontend/src/routes/UserRouter.tsx
@@ -1,12 +1,12 @@
 import { Routes, Route } from 'react-router-dom';
-import Join from '../pages/Join/Join';
-import Login from '../pages/Login/Login';
+import { Join } from '../pages/Join/Join';
+import { Login } from '../pages/Login/Login';
 
 export const UserRouter = () => {
   return (
     <Routes>
-      <Route path="/login" element={<Login></Login>} />
-      <Route path="/join" element={<Join></Join>} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/join" element={<Join />} />
       <Route path="/find-email" element={<div>찾기</div>} />
       <Route path="/find-password" element={<div>찾기</div>} />
     </Routes>
