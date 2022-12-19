8ccd0acef8cd6496e0d378dc94c870fc1e1e65da
LucetTin5 <lucettin5@gmail.com>
2022-12-19 23:22:45 +0900

    Chore: 사용되지 않는 import 제거

--- Frontend/src/components/Join/JoinForm.tsx
+++ Frontend/src/components/Join/JoinForm.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { FormProvider, useForm } from 'react-hook-form';
 import { UserForm } from '../UserForm/styles';
