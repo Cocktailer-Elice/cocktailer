60194c47810ffcfdcc37397ff6d1ee84e0a34ac0
LucetTin5 <lucettin5@gmail.com>
2022-12-20 13:42:43 +0900

    feat: mypage route 추가

--- Frontend/src/routes/UserRouter.tsx
+++ Frontend/src/routes/UserRouter.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Join from '../pages/Join/Join';
 import Login from '../pages/Login/Login';
+import { Mypage } from '../pages/Mypage/Mypage';
 
 export const UserRouter = () => {
   return (
@@ -9,6 +10,7 @@ export const UserRouter = () => {
       <Route path="/join" element={<Join></Join>} />
       <Route path="/find-email" element={<div>찾기</div>} />
       <Route path="/find-password" element={<div>찾기</div>} />
+      <Route path="/mypage" element={<Mypage />} />
     </Routes>
   );
 };
