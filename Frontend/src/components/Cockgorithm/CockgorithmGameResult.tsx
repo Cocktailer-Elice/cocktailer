bd2fbec27e04dd5ecc4a75c1527b71f5d97ecdd1
JongHoonSon <roka1684@gmail.com>
2022-12-21 13:20:37 +0900

    Feat: CocktailTitle, CocktailDetailInfo 컴포넌트 생성

--- Frontend/src/components/Cockgorithm/CockgorithmGameResult.tsx
+++ Frontend/src/components/Cockgorithm/CockgorithmGameResult.tsx
@@ -9,15 +9,41 @@ interface CockgorithmGameResultProps {
 export const CockgorithmGameResult = ({
   cocktailInfo,
 }: CockgorithmGameResultProps) => {
-  return <GameResult>"{cocktailInfo}"를 추천드려요!</GameResult>;
+  return (
+    <GameResult>
+      <CocktailTitle>"{cocktailInfo}"를 추천드려요!</CocktailTitle>
+      <CocktailDetailInfo>{cocktailInfo} 상세정보</CocktailDetailInfo>
+    </GameResult>
+  );
 };
 
 const GameResult = styled.div`
   width: 100%;
   height: 450px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   color: whitesmoke;
   background-color: tomato;
+`;
+
+const CocktailTitle = styled.div`
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: brown;
+`;
+
+const CocktailDetailInfo = styled.div`
+  width: 100%;
+  height: 400px;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  background-color: red;
 `;
