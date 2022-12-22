09d4df547a4424e5308b1ba41540fd132a27b0db
siha <es2008e@naver.com>
2022-12-22 16:01:49 +0900

    Design : 카테고리 리스트 페이지 구조

--- Frontend/src/containers/Cockcipe/List/CategoryContainer.tsx
+++ Frontend/src/containers/Cockcipe/List/CategoryContainer.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { CocktailListItem } from '../../../components/Cockcipe/List/CocktailListItem';
+import { SearchCocktailInput } from '../../../components/Cockcipe/List/SearchCocktailInput';
 
 // TODO : 카테고리별 아이템 출력하기
 export const CategoryContainer = () => {
@@ -10,5 +13,20 @@ export const CategoryContainer = () => {
       .get(`http://localhost:8000/api/cocktails/category=${categoryId}`)
       .then((res) => console.log(res));
   }, []);
-  return <div>CategoryContainer</div>;
+  return (
+    <>
+      <CategoryHeader>{categoryId}</CategoryHeader>
+      <SearchCocktailInput />
+      <CocktailListItem />
+      <CocktailListItem />
+      <CocktailListItem />
+      <CocktailListItem />
+    </>
+  );
 };
+
+const CategoryHeader = styled.div`
+  font-size: 30px;
+  text-align: center;
+  margin: 20px 0;
+`;
