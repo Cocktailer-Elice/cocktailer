c5a6b4c8f5687476a87648aa60bb80adf695b298
LucetTin5 <lucettin5@gmail.com>
2022-12-23 18:42:20 +0900

    feat: 비밀번호 변경 로그인only로 수정

--- Frontend/src/pages/Mypage/Edit/EditPassword.tsx
+++ Frontend/src/pages/Mypage/Edit/EditPassword.tsx
@@ -1,5 +1,15 @@
 import { EditPasswordContainer } from '../../../containers/Edits/EditPasswordContainer';
+import { loginChecker } from '../../../utils/loginChecker';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const EditPassword = () => {
+  const isLoggedIn = loginChecker();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isLoggedIn) {
+      navigate('/login');
+    }
+  }, [isLoggedIn]);
   return <EditPasswordContainer />;
 };
