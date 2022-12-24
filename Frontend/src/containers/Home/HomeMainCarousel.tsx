1917bafb872470861e478d34e404ed1c72511469
JongHoonSon <roka1684@gmail.com>
2022-12-24 11:38:18 +0900

    Feat: Slider 모듈 적용

--- Frontend/src/containers/Home/HomeMainCarousel.tsx
+++ Frontend/src/containers/Home/HomeMainCarousel.tsx
@@ -1,9 +1,39 @@
 import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../../swiper.css';
 
 export const HomeMainCarousel = () => {
   return (
     <MainCarousel>
-      <span>홈 메인 캐러셀</span>
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
     </MainCarousel>
   );
 };
