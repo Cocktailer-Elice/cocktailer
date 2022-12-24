c8b9672857cf303305a4c86531381eb82cecca05
JongHoonSon <roka1684@gmail.com>
2022-12-24 11:45:01 +0900

    Feat: SectionHeader 컴포넌트 적용

--- Frontend/src/pages/Home/Home.tsx
+++ Frontend/src/pages/Home/Home.tsx
@@ -13,9 +13,11 @@ export const Home = () => {
       <Section>
         <HomeWidgetsContainer />
       </Section>
+      <SectionHeader>칵테일 레시피 랭킹 TOP 10</SectionHeader>
       <Section>
         <HomeSubCarousel />
       </Section>
+      <SectionHeader>유저 랭킹 TOP 10</SectionHeader>
       <Section>
         <HomeSubCarousel />
       </Section>
@@ -36,4 +38,14 @@ const Section = styled.div`
   height: 20%;
   border: 1px solid gray;
   margin-bottom: 10px;
+`;
+
+const SectionHeader = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 0 10px;
+  font-size: 16px;
 `;
