2a332478c2f3a6abfc040e5508ecc2b5ede1af85
JongHoonSon <roka1684@gmail.com>
2022-12-27 01:22:42 +0900

    Design: 바텐더 인증 배지 적용

--- Frontend/src/components/Home/HomeCocktailRanking.tsx
+++ Frontend/src/components/Home/HomeCocktailRanking.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 import { CocktailRanking } from '../../../../types';
-import { CockflowBadge } from './../Cockflow/CockflowBadge';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 interface HomeCocktailRankingProps {
   cocktailRankingInfo: CocktailRanking;
@@ -25,7 +25,7 @@ export const HomeCocktailRanking = ({
         </CocktailLikes>
         <OwnerInfo>
           <OwnerName>{cocktailRankingInfo.owner.nickname}</OwnerName>
-          {cocktailRankingInfo.owner.isBartender && <CockflowBadge />}
+          {cocktailRankingInfo.owner.isBartender && <CustomCheckCircleIcon />}
         </OwnerInfo>
       </Link>
     </Container>
@@ -40,7 +40,7 @@ const Container = styled.div`
 const CocktailImage = styled.img`
   width: 100%;
   height: 60%;
-  background-color: ${(props) => props.theme.colors.indigo4};
+  background-color: ${(props) => props.theme.colors.indigo1};
   border-radius: 5px;
 `;
 
@@ -54,13 +54,14 @@ const CocktailInfo = styled.div`
 const CocktailName = styled.div`
   font-size: 14px;
   font-weight: bold;
-  color: ${(props) => props.theme.colors.indigo5};
+  color: ${(props) => props.theme.colors.indigo7};
 `;
 
 const CustomThumbUpIcon = styled(ThumbUpIcon)`
   font-size: 12px;
   margin-left: 5px;
   margin-right: 3px;
+  color: ${(props) => props.theme.colors.indigo4};
 `;
 
 const CocktailLikes = styled.div`
@@ -70,6 +71,7 @@ const CocktailLikes = styled.div`
   font-size: 11px;
   font-weight: bold;
   margin-top: 8px;
+  color: ${(props) => props.theme.colors.indigo4};
 `;
 
 const OwnerInfo = styled.div`
@@ -81,6 +83,12 @@ const OwnerInfo = styled.div`
 
 const OwnerName = styled.div`
   font-size: 12px;
-  color: rgba(0, 0, 0, 0.5);
+  color: rgba(0, 0, 0, 0.7);
   font-weight: 600;
+`;
+
+const CustomCheckCircleIcon = styled(CheckCircleIcon)`
+  margin-left: 3px;
+  font-size: 15px;
+  color: ${(props) => props.theme.colors.indigo4};
 `;
