779f5a825e914d27ad12eb85ff5b1dada6e34270
JongHoonSon <roka1684@gmail.com>
2022-12-27 05:25:45 +0900

    Design: contentMenus 이모지 추가

--- Frontend/src/constants/pages.ts
+++ Frontend/src/constants/pages.ts
@@ -1,5 +1,5 @@
 export const contentMenus = [
-  { pageName: '칵시피', link: '/cockcipe' },
-  { pageName: '칵플로우', link: '/cockflow' },
-  { pageName: '칵고리즘', link: '/cockgorithm' },
+  { pageName: '🍹 칵시피', link: '/cockcipe' },
+  { pageName: '🙋 칵플로우', link: '/cockflow' },
+  { pageName: '🛸 칵고리즘', link: '/cockgorithm' },
 ];
