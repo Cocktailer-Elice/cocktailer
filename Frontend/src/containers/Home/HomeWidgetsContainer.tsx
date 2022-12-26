978ed2caa2ece16097b22cb4e07d5e37f786228f
JongHoonSon <roka1684@gmail.com>
2022-12-26 17:39:42 +0900

    Feat: Widget key 값 부여

--- Frontend/src/containers/Home/HomeWidgetsContainer.tsx
+++ Frontend/src/containers/Home/HomeWidgetsContainer.tsx
@@ -7,8 +7,8 @@ import { contentMenus } from './../../constants/pages';
 export const HomeWidgetsContainer = () => {
   return (
     <Container>
-      {contentMenus.map((contentMenu) => (
-        <Link to={contentMenu.link}>
+      {contentMenus.map((contentMenu, index) => (
+        <Link key={index} to={contentMenu.link}>
           <HomeWidget title={contentMenu.pageName} />
         </Link>
       ))}
