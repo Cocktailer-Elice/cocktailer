a350058fedcdb7de4f58a83bb264ec39d5114ce5
JongHoonSon <roka1684@gmail.com>
2022-12-27 00:41:46 +0900

    Design: 칵테일 레시피 랭킹 스타일 적용

--- Frontend/src/components/Home/HomeCocktailRanking.tsx
+++ Frontend/src/components/Home/HomeCocktailRanking.tsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
+
 import { CocktailRanking } from '../../../../types';
+import { CockflowBadge } from './../Cockflow/CockflowBadge';
 
 interface HomeCocktailRankingProps {
   cocktailRankingInfo: CocktailRanking;
@@ -10,10 +14,20 @@ export const HomeCocktailRanking = ({
 }: HomeCocktailRankingProps) => {
   return (
     <Container>
-      <Image />
-      <Title>
-        <Name>{cocktailRankingInfo.name}</Name>
-      </Title>
+      <Link to={`/cockcipe/detail/${cocktailRankingInfo.id}`}>
+        <CocktailImage src={cocktailRankingInfo.img} />
+        <CocktailInfo>
+          <CocktailName>{cocktailRankingInfo.name}</CocktailName>
+        </CocktailInfo>
+        <CocktailLikes>
+          <CustomThumbUpIcon />
+          {cocktailRankingInfo.likes}
+        </CocktailLikes>
+        <OwnerInfo>
+          <OwnerName>{cocktailRankingInfo.owner.nickname}</OwnerName>
+          {cocktailRankingInfo.owner.isBartender && <CockflowBadge />}
+        </OwnerInfo>
+      </Link>
     </Container>
   );
 };
@@ -23,21 +37,50 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Image = styled.img`
+const CocktailImage = styled.img`
   width: 100%;
   height: 60%;
-  background-color: rgba(0, 0, 0, 0.1);
+  background-color: ${(props) => props.theme.colors.indigo4};
   border-radius: 5px;
-  margin-bottom: 5px;
 `;
 
-const Title = styled.div`
+const CocktailInfo = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 10px;
 `;
 
-const Name = styled.div`
+const CocktailName = styled.div`
   font-size: 14px;
   font-weight: bold;
+  color: ${(props) => props.theme.colors.indigo5};
+`;
+
+const CustomThumbUpIcon = styled(ThumbUpIcon)`
+  font-size: 12px;
+  margin-left: 5px;
+  margin-right: 3px;
+`;
+
+const CocktailLikes = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 11px;
+  font-weight: bold;
+  margin-top: 8px;
+`;
+
+const OwnerInfo = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 8px;
+`;
+
+const OwnerName = styled.div`
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.5);
+  font-weight: 600;
 `;
