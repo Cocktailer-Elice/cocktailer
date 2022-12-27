8c190214a32d1a16ce26fd86b7d02d98ac4ef11a
LucetTin5 <lucettin5@gmail.com>
2022-12-27 22:26:14 +0900

    feat: 비밀번호 변경 필요 시 이동하도록 변경

--- Frontend/src/common/withLogin.tsx
+++ Frontend/src/common/withLogin.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useAuthentication } from '../hooks/useAuthentication';
-// import { useCurrentUser } from '../hooks/useCurrentUser';
+import { useCurrentUser } from '../hooks/useCurrentUser';
 import { Login } from '../pages/Login/Login';
 
 interface WrappedProps {}
@@ -9,12 +9,12 @@ export const withLogin =
   <T extends WrappedProps>(WrappedComponent: React.ComponentType<T>) =>
   (props: T) => {
     const isLoggedIn = useAuthentication();
-    // const user = useCurrentUser();
-    // if (user.needPasswordChange) {
-    //   alert('비밀번호 변경이 필요합니다.');
-    //   navigate('/mypage/change-password');
-    // }
+    const user = useCurrentUser();
     const navigate = useNavigate();
+    if (user?.isPasswordTemporary) {
+      alert('비밀번호 변경이 필요합니다.');
+      navigate('/mypage/change-password');
+    }
     if (isLoggedIn) {
       return <WrappedComponent {...props} />;
     } else {
