6f7a1dd35c6f2cc79c522b84cd7a050a543ff49f
BB2e <79270369+BB2e@users.noreply.github.com>
2022-12-29 01:33:04 +0900

    fix: 이미지 새로고침 수정

---
+++ Frontend/src/components/Cockflow/CockflowImgWrap.tsx
@@ -0,0 +1,34 @@
+import { CockflowItemBox } from "./CockflowItemBox";
+import { slice20 } from '../../components/Cockflow/CockflowUtils';
+import styled from "styled-components";
+
+const imgS3 = (index: number) => {
+    // const num = Math.round(Math.random() * 20);
+    const num = (index % 20);
+    return `https://cockflow.s3.ap-northeast-1.amazonaws.com/CockflowList/img${num}.jpg`;
+}
+
+interface TypeItem {
+    item: {
+        id: string,
+        title: string
+    },
+    index: number
+}
+
+export const CockflowImgWrap = ({ item, index }: TypeItem) => {
+    return (
+        <Item key={item.id}>
+            <CockflowItemBox key={item.id} id={item.id} title={slice20(item.title)}
+                content={imgS3(index)} />
+        </Item>
+    )
+}
+
+const Item = styled.li`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 28.9px;
+`;
+
+// imgArr[Math.round(Math.random() * (imgArr.length - 1))]
--- Frontend/src/pages/Cockflow/CockflowPage.tsx
+++ Frontend/src/pages/Cockflow/CockflowPage.tsx
@@ -3,29 +3,13 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { CockflowLinkBtn } from '../../components/Cockflow/CockflowLinkBtn';
 import { CockflowHeader } from '../../components/Cockflow/CockflowHeader';
-import { CockflowItemBox } from '../../components/Cockflow/CockflowItemBox';
 import { P5 } from '../../components/Cockflow/style';
-import { slice20 } from '../../components/Cockflow/CockflowUtils';
 import { useInView } from 'react-intersection-observer';
 import { Container } from '../../components/Cockflow/style';
 import { Helmet } from 'react-helmet';
 import { useAuthentication } from '../../hooks/useAuthentication';
 import { GET_COCKFLOW } from '../../constants/api';
-
-
-const imgArr = [
-  'https://cdn.pixabay.com/photo/2013/02/21/19/06/drink-84533_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2013/02/21/19/06/drink-84533_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2016/01/02/04/58/martini-1117932_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2015/11/29/01/38/ice-1068233_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2018/04/08/13/17/glass-3301200_960_720.jpg',
-  'https://cdn.pixabay.com/photo/2018/03/06/07/01/drink-3202709__340.jpg',
-  'https://cdn.pixabay.com/photo/2015/07/23/19/23/cocktail-857393__340.jpg',
-  'https://cdn.pixabay.com/photo/2018/03/18/18/54/drink-3237895__340.jpg'
-];
-
-console.log(`https://cockflow.s3.ap-northeast-1.amazonaws.com/CockflowList/`)
-// https://cockflow.s3.ap-northeast-1.amazonaws.com/CockflowList/img0.jpg
+import { CockflowImgWrap } from '../../components/Cockflow/CockflowImgWrap';
 
 export const CockflowPage = () => {
   const [listData, setListData] = useState([{
@@ -34,13 +18,9 @@ export const CockflowPage = () => {
   }]);
 
   const [loading, setLoading] = useState(false);
-
   const [page, setPage] = useState(1);
-
   const [maxPage, setMaxpage] = useState(1);
-
   const [ref, inView] = useInView();
-
   const isLoggedIn = useAuthentication();
 
   const getList = () => {
@@ -66,32 +46,23 @@ export const CockflowPage = () => {
     }
   }, [inView]);
 
-  useEffect(() => {
-    // const data = axios.get(`https://cockflow.s3.ap-northeast-1.amazonaws.com/CockflowList/`)
-    // console.log(data)
-
-  }, [])
-
   return (
     <Container>
       <Helmet>
         <title>Cocktailer | 칵플로우</title>
       </Helmet>
-      <P5>
+      <ListWrapper>
         <CockflowHeader />
         {
           isLoggedIn && <CockflowLinkBtn link="/cockflow/new" title="질문하기" />
         }
         <List>
-          {listData.map(item =>
-            <Item key={item.id}>
-              <CockflowItemBox key={item.id} id={item.id} title={slice20(item.title)}
-                content={imgArr[Math.round(Math.random() * (imgArr.length - 1))]} />
-            </Item>
+          {listData.map((item, index) =>
+            <CockflowImgWrap item={item} index={index} />
           )}
         </List>
-        <p ref={ref}></p>
-      </P5>
+        <Pointer ref={ref}></Pointer>
+      </ListWrapper>
     </Container>
   );
 };
@@ -103,8 +74,11 @@ const List = styled.ul`
   column-gap: 20px;
 `;
 
-const Item = styled.li`
-  display: flex;
-  flex-wrap: wrap;
-  margin-bottom: 28.9px;
+const ListWrapper = styled(P5)`
+  position: relative;
+`;
+
+const Pointer = styled.p`
+  position: absolute;
+  bottom: 15%;
 `;
