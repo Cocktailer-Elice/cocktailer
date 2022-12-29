42f6fccc3c045a56e9f66a08a0f3edfd7d0d601b
InGukOh <87802273+InGukOh@users.noreply.github.com>
2022-12-29 14:08:22 +0900

    Change : 예상에러 변경

--- Backend/src/routers/middlewares/validations/cocktailValidation.ts
+++ Backend/src/routers/middlewares/validations/cocktailValidation.ts
@@ -4,7 +4,7 @@ import Joi from 'joi';
 const cocktailValidation = Joi.object({
   category: Joi.string().min(1).max(200),
   name: Joi.string().min(1).max(20),
-  flavor: Joi.array().min(1).max(10),
+  flavor: Joi.array().min(1).max(11),
   degree: Joi.number().integer().min(1).max(100),
   img: Joi.string(),
   ratio: Joi.object(),
